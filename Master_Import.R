bbd46a62f5503dcf0c01b145f120578ecd549bde
Kevin Bonds <Kevin_Bonds@gap.com>
2016-12-05 16:29:47 -0800

    Add all SOT and OTS Create tables

--- Master_Import.R
+++ Master_Import.R
@@ -6,6 +6,7 @@ library(formattable)
 library(RJDBC)
 library(rChoiceDialogs)
 
+# Choose file directory ----
 choose_file_directory <- function()
 {
   v <- jchoose.dir()
@@ -20,30 +21,31 @@ my_connect <- odbcConnect(dsn= "IP EDWP", uid= my_uid, pwd= my_pwd)
 # sqlTables(my_connect, catalog = "EDWP", tableName  = "tables")
 sqlQuery(my_connect, query = "SELECT  * from dbc.dbcinfo;")
 
-
-
-
-# Create RJDBC connection - In Dev----
+# Create RJDBC connection - In Dev
 #Sys.setenv(JAVA_HOME= "C:\\Users\\Ke2l8b1\\Documents\\Teradata\\JDBC_Driver\\jre-8u101-windows-x64.exe")
 # drv2 <- JDBC("com.teradata.jdbc.TeraConnectionPoolDataSource", "C:\\Users\\Ke2l8b1\\Documents\\Teradata\\JDBC_Driver\\terajdbc4.jar;C:\\Users\\Ke2l8b1\\Documents\\Teradata\\JDBC_Driver\\tdgssconfig.jar")
 # conn <- dbConnect(drv2, "jdbc:teradata://tdprodcop1.gap.com", my_uid, my_pwd)
 # SOT_Master_RJDBC <- dbGetQuery(conn, 
 #                       query = "SELECT  * from dbc.dbcinfo;")
 
-
+# Query EDW ----
 SOT_Master <- sqlQuery(my_connect, 
                        query = "SELECT  * from SRAA_SAND.VIEW_SOT_MASTER;")
 
 OTS_Master <- sqlQuery(my_connect, 
                        query = "SELECT  * from SRAA_SAND.VIEW_OTS_MASTER;")
+# save Master Objects ----
+save(SOT_Master, file = "SOT_Master_object.rtf")
+save(OTS_Master, file = "OTS_Master_object.rtf")
 
 # load("C:\\Users\\Ke2l8b1\\Documents\\SOT Weekly\\2016\\Wk43\\SOT_Master_object.rtf")
 # load("C:\\Users\\Ke2l8b1\\Documents\\SOT Weekly\\2016\\Wk43\\OTS_Master_object.rtf")
 
 source("SOT_OTS_Custom_Functions.R")
 
+OTS_Master %>%  subset(Parent_Vendor == "Liberty Distrubution")
 
-# Monthly SOT Brand and Category Table ----
+# Create Monthly SOT Brand and Category Table ----
 Monthly_Brand_Category_SOT <- SOT_Master %>%
   filter(SOT_Master$ShipCancelWeek <= 43) %>%
   group_by(ShipCancelMonth, ReportingBrand, Category) %>% 
@@ -70,8 +72,7 @@ Monthly_Brand_Category_SOT <- SOT_Master %>%
          WTPPASOTLateUnits)
 View(Monthly_Brand_Category_SOT)
 
-
-# Monthly SOT Brand Table ----
+# Create Monthly SOT Brand Table ----
 Monthly_Brand_SOT <- SOT_Master %>%
   filter(SOT_Master$ShipCancelWeek <= 43) %>%
   group_by(ShipCancelMonth, ReportingBrand) %>% 
@@ -97,7 +98,7 @@ Monthly_Brand_SOT <- SOT_Master %>%
          WTPPASOTLateUnits)
 View(Monthly_Brand_SOT)
 
-# Monthly SOT Category Table ----
+# Create Monthly SOT Category Table ----
 Monthly_Category_SOT <- SOT_Master %>%
   filter(SOT_Master$ShipCancelWeek <= 43) %>%
   group_by(ShipCancelMonth, Category) %>% 
@@ -123,6 +124,110 @@ Monthly_Category_SOT <- SOT_Master %>%
          WTPPASOTLateUnits)
 View(Monthly_Category_SOT) 
 
+# Create Monthly Gap Inc SOT Table ----
+Monthly_GapInc_SOT <- SOT_Master %>%
+  filter(SOT_Master$ShipCancelWeek <= 43) %>%
+  group_by(ShipCancelMonth) %>% 
+  summarise("SOTUnits" = floor(sum(Units[Lateness== "OnTime" | Lateness== "Late"])),
+            "SOTOnTimeUnits" = floor(sum(Units[Lateness=="OnTime"])),
+            "SOTLateUnits"= floor(sum(Units[Lateness=="Late"])),
+            "SOTLate5daysUnits" = floor(sum(Units[Lateness=="Late" & DAYS_LATE > 5])), 
+            "WTSOTLateUnits" = floor(sum(Units[Lateness=="Late"]*DAYS_LATE[Lateness=="Late" & DAYS_LATE >=1])),
+            "PPAUnits" = floor(sum(Units[SHP_MODE_CATG_NM == "PrepaidAir"])),
+            "PPASOTLateUnits" = floor(sum(Units[SHP_MODE_CATG_NM == "PrepaidAir" & Lateness=="Late"])), 
+            "PPASOT5daysLateUnits" = floor(sum(Units[SHP_MODE_CATG_NM == "PrepaidAir" & Lateness=="Late" & DAYS_LATE>5])),
+            "WTPPASOTLateUnits" = floor(sum(Units[SHP_MODE_CATG_NM == "PrepaidAir" & Lateness=="Late"]*DAYS_LATE[SHP_MODE_CATG_NM == "PrepaidAir" & Lateness=="Late" & DAYS_LATE >=1]))) %>%  
+  select(ShipCancelMonth, 
+         SOTUnits, 
+         SOTOnTimeUnits, 
+         SOTLateUnits, 
+         SOTLate5daysUnits, 
+         WTSOTLateUnits, 
+         PPAUnits,
+         PPASOTLateUnits,
+         PPASOT5daysLateUnits,
+         WTPPASOTLateUnits)
+View(Monthly_GapInc_SOT) 
+
+# Create Monthly OTS Brand and Category Table ----
+Monthly_Brand_Category_OTS <- OTS_Master %>%
+  filter(OTS_Master$Week <= 43) %>%
+  group_by(Month_Number, ReportingBrand, Category) %>% 
+  summarise("OTSUnits" = floor(sum(Units[Lateness== "OnTime" | Lateness== "Late"])),
+            "OTSOnTimeUnits" = floor(sum(Units[Lateness=="OnTime"])),
+            "OTSLateUnits"= floor(sum(Units[Lateness=="Late"])),
+            "OTSLate5daysUnits" = floor(sum(Units[Lateness=="Late" & Days_Late > 5])), 
+            "WTOTSLateUnits" = floor(sum(Units[Lateness=="Late"]*Days_Late[Lateness=="Late" & Days_Late >=1])),
+            "PPAOTSLateUnits" = floor(sum(Units[SHP_MODE_CATG_NM == "PrepaidAir" & Lateness=="Late"]))) %>%  
+  select(Month_Number, 
+         ReportingBrand, 
+         Category, 
+         OTSUnits, 
+         OTSOnTimeUnits, 
+         OTSLateUnits, 
+         OTSLate5daysUnits, 
+         WTOTSLateUnits, 
+         PPAOTSLateUnits)
+View(Monthly_Brand_Category_OTS)
+
+# Create Monthly OTS Brand Table ----
+Monthly_Brand_OTS <- OTS_Master %>%
+  filter(OTS_Master$Week <= 43) %>%
+  group_by(Month_Number, ReportingBrand) %>% 
+  summarise("OTSUnits" = floor(sum(Units[Lateness== "OnTime" | Lateness== "Late"])),
+            "OTSOnTimeUnits" = floor(sum(Units[Lateness=="OnTime"])),
+            "OTSLateUnits"= floor(sum(Units[Lateness=="Late"])),
+            "OTSLate5daysUnits" = floor(sum(Units[Lateness=="Late" & Days_Late > 5])), 
+            "WTOTSLateUnits" = floor(sum(Units[Lateness=="Late"]*Days_Late[Lateness=="Late" & Days_Late >=1])),
+            "PPAOTSLateUnits" = floor(sum(Units[SHP_MODE_CATG_NM == "PrepaidAir" & Lateness=="Late"]))) %>%  
+  select(Month_Number, 
+         ReportingBrand, 
+         OTSUnits, 
+         OTSOnTimeUnits, 
+         OTSLateUnits, 
+         OTSLate5daysUnits, 
+         WTOTSLateUnits, 
+         PPAOTSLateUnits)
+View(Monthly_Brand_OTS)
+
+# Create Monthly OTS Category Table ----
+Monthly_Category_OTS <- OTS_Master %>%
+  filter(OTS_Master$Week <= 43) %>%
+  group_by(Month_Number, Category) %>% 
+  summarise("OTSUnits" = floor(sum(Units[Lateness== "OnTime" | Lateness== "Late"])),
+            "OTSOnTimeUnits" = floor(sum(Units[Lateness=="OnTime"])),
+            "OTSLateUnits"= floor(sum(Units[Lateness=="Late"])),
+            "OTSLate5daysUnits" = floor(sum(Units[Lateness=="Late" & Days_Late > 5])), 
+            "WTOTSLateUnits" = floor(sum(Units[Lateness=="Late"]*Days_Late[Lateness=="Late" & Days_Late >=1])),
+            "PPAOTSLateUnits" = floor(sum(Units[SHP_MODE_CATG_NM == "PrepaidAir" & Lateness=="Late"]))) %>%  
+  select(Month_Number, 
+         Category, 
+         OTSUnits, 
+         OTSOnTimeUnits, 
+         OTSLateUnits, 
+         OTSLate5daysUnits, 
+         WTOTSLateUnits, 
+         PPAOTSLateUnits)
+View(Monthly_Category_OTS)
+
+# Create Monthly Gap Inc OTS Table ----
+Monthly_GapInc_OTS <- OTS_Master %>%
+  filter(OTS_Master$Week <= 43) %>%
+  group_by(Month_Number) %>% 
+  summarise("OTSUnits" = floor(sum(Units[Lateness== "OnTime" | Lateness== "Late"])),
+            "OTSOnTimeUnits" = floor(sum(Units[Lateness=="OnTime"])),
+            "OTSLateUnits"= floor(sum(Units[Lateness=="Late"])),
+            "OTSLate5daysUnits" = floor(sum(Units[Lateness=="Late" & Days_Late > 5])), 
+            "WTOTSLateUnits" = floor(sum(Units[Lateness=="Late"]*Days_Late[Lateness=="Late" & Days_Late >=1])),
+            "PPAOTSLateUnits" = floor(sum(Units[SHP_MODE_CATG_NM == "PrepaidAir" & Lateness=="Late"]))) %>%  
+  select(Month_Number, 
+         OTSUnits, 
+         OTSOnTimeUnits, 
+         OTSLateUnits, 
+         OTSLate5daysUnits, 
+         WTOTSLateUnits, 
+         PPAOTSLateUnits)
+View(Monthly_GapInc_OTS)
 
 # Experimental section ----
 On_Time_Stock_table <- OTS_Master %>% 
